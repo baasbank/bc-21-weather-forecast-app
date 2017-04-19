75c23849128fa82e1d43083670dfdb69a83ec0b1
Baasbank Akinmuleya <baasb4real@yahoo.com>
2017-04-19 15:39:54 +0100

    Code refactoring

--- index.js
+++ index.js
@@ -39,13 +39,13 @@ function getForecast() {
                  //Display movie details
                 console.log(chalk.green('\n Here is the forecast for ' + answer.cityname.toUpperCase() + ': '));
                 console.log(chalk.yellow('Title: ') + data.query.results.channel.description);
-                console.log((chalk.yellow('Wind: ')) + (chalk.blue('Wind Chill; ')) + data.query.results.channel.wind.chill + ', ' + (chalk.blue('Wind Direction; ')) + data.query.results.channel.wind.direction + ', ' + (chalk.blue('Wind Speed; ')) + data.query.results.channel.wind.speed);
-                console.log((chalk.yellow('Atmosphere: ')) + (chalk.blue('Humidity; ')) + data.query.results.channel.atmosphere.humidity + ', ' + (chalk.blue('Pressure; ')) + data.query.results.channel.atmosphere.pressure + ', ' + (chalk.blue('Visibility; ')) + data.query.results.channel.atmosphere.visibility);
-                console.log((chalk.yellow('Astronomy: ')) + (chalk.blue('Sunrise at about; ')) + data.query.results.channel.astronomy.sunrise + ', ' + (chalk.blue('Sunset at about; ')) + data.query.results.channel.astronomy.sunset);
-                console.log((chalk.yellow('Geographical Coordinates: ')) + (chalk.blue('Longitude; ')) + data.query.results.channel.item.long + ', ' + (chalk.blue('Latitude; ')) + data.query.results.channel.item.lat);
-                console.log((chalk.yellow('Temperature: ')) + (chalk.blue('Low; ')) + data.query.results.channel.item.forecast[0].low + ', ' + (chalk.blue('High; ')) + data.query.results.channel.item.forecast[0].high);
+                console.log((chalk.yellow('Wind: ')) + (chalk.green('Wind Chill; ')) + data.query.results.channel.wind.chill + ', ' + (chalk.green('Wind Direction; ')) + data.query.results.channel.wind.direction + ', ' + (chalk.green('Wind Speed; ')) + data.query.results.channel.wind.speed);
+                console.log((chalk.yellow('Atmosphere: ')) + (chalk.green('Humidity; ')) + data.query.results.channel.atmosphere.humidity + ', ' + (chalk.green('Pressure; ')) + data.query.results.channel.atmosphere.pressure + ', ' + (chalk.green('Visibility; ')) + data.query.results.channel.atmosphere.visibility);
+                console.log((chalk.yellow('Astronomy: ')) + (chalk.green('Sunrise at about; ')) + data.query.results.channel.astronomy.sunrise + ', ' + (chalk.green('Sunset at about; ')) + data.query.results.channel.astronomy.sunset);
+                console.log((chalk.yellow('Geographical Coordinates: ')) + (chalk.green('Longitude; ')) + data.query.results.channel.item.long + ', ' + (chalk.green('Latitude; ')) + data.query.results.channel.item.lat);
+                console.log((chalk.yellow('Temperature: ')) + (chalk.green('Low; ')) + data.query.results.channel.item.forecast[0].low + ', ' + (chalk.green('High; ')) + data.query.results.channel.item.forecast[0].high);
                 console.log(chalk.yellow('Condition: ') + data.query.results.channel.item.condition.text);
-                console.log(chalk.yellow("Tomorrow's Forecast: "));
+                /*console.log(chalk.yellow("Tomorrow's Forecast: "));
                 console.log(chalk.yellow('Day/Date: ') + data.query.results.channel.item.forecast[1].day + ', ' + data.query.results.channel.item.forecast[1].date);
                 console.log((chalk.yellow('Temperature: ')) + (chalk.blue('Low; ')) + data.query.results.channel.item.forecast[1].low + ', ' + (chalk.blue('High; ')) + data.query.results.channel.item.forecast[1].high);
                 console.log(chalk.yellow('Condition: ') + data.query.results.channel.item.forecast[1].text);
@@ -72,12 +72,54 @@ function getForecast() {
                 console.log(chalk.yellow('Condition: ') + data.query.results.channel.item.forecast[8].text);
                 console.log(chalk.yellow('Forecast for: ') + data.query.results.channel.item.forecast[9].day + ', ' + data.query.results.channel.item.forecast[9].date);
                 console.log((chalk.yellow('Temperature: ')) + (chalk.blue('Low; ')) + data.query.results.channel.item.forecast[9].low + ', ' + (chalk.blue('High; ')) + data.query.results.channel.item.forecast[9].high);
-                console.log(chalk.yellow('Condition: ') + data.query.results.channel.item.forecast[9].text);
+                console.log(chalk.yellow('Condition: ') + data.query.results.channel.item.forecast[9].text);*/
                                 
                 exitApp();                 
             });
         });
 }
+
+function getCoordinates() {
+    inquirer.prompt([ {
+            type: 'input',
+            name: 'cityname',
+            message: 'What city do you want coordinates for?'
+        },
+
+        ]).then(function (answer) {
+            //Reformat movie title to fit query
+            var cityname    = answer.cityname.split(/\s+/).join('%20');
+            var link = ' https://query.yahooapis.com/v1/public/yql?q=select%20*%20from%20weather.forecast%20where%20woeid%20in%20(select%20woeid%20from%20geo.places(1)%20where%20text%3D%22'+cityname+'%22)&format=json&env=store%3A%2F%2Fdatatables.org%2Falltableswithkeys';
+
+
+            var status = new Spinner("I'll get your coordinates in a jiffy...");
+            status.start();
+            request(link, function (err, res, data){
+                if (err) {
+                    return console.log(err);
+                } 
+
+                data = JSON.parse(data);
+                status.stop();
+
+                //Exit if nothing was found
+                if (data === '') {
+                    console.log('\nNothing found.\n');
+                    return exitApp();
+                } 
+
+                 //Display movie details
+                console.log(chalk.green('\n Here are the coordinates for ' + answer.cityname.toUpperCase() + ': '));
+                
+               
+                console.log((chalk.yellow('Geographical Coordinates: ')) + (chalk.blue('Longitude; ')) + data.query.results.channel.item.long + ', ' + (chalk.blue('Latitude; ')) + data.query.results.channel.item.lat);
+                exitApp();
+
+              
+            });
+        });
+}
+            
 
 //Function that starts the application
 function startApp() {
@@ -88,12 +130,17 @@ function startApp() {
     choices: [
       //'Search for a book (e.g: Things fall apart)',
       //'Search for a movie (e.g: The expendables)',
-      'Get weather forecasts',
+      'Get weather forecast',
+      'Get geographical coordinates'
     ]
   } ]).then(function (answer) {
 
-        if(answer.option === 'Get weather forecasts'){
+        if(answer.option === 'Get weather forecast') {
             getForecast();
+        }
+
+        if(answer.option === 'Get geographical coordinates') {
+          getCoordinates();
         }
 
     });
