a99a2575fd321b2df47ed94e55df40274bd7dbdb
Baasbank Akinmuleya <baasb4real@yahoo.com>
2017-04-19 13:22:03 +0100

    Get user input and send same to the weather API

--- index.js
+++ index.js
@@ -13,3 +13,8 @@ function getForecast() {
             name: 'cityname',
             message: 'What city do you want forecasts for?'
         },
+
+        ]).then(function (answer) {
+            //Reformat movie title to fit query
+            var cityname    = answer.cityname.split(/\s+/).join('%20');
+            var link = ' https://query.yahooapis.com/v1/public/yql?q=select%20*%20from%20weather.forecast%20where%20woeid%20in%20(select%20woeid%20from%20geo.places(1)%20where%20text%3D%22'+cityname+'%22)&format=json&env=store%3A%2F%2Fdatatables.org%2Falltableswithkeys';
